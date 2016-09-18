d6a1acd8fcb12ef29d100875c35914b91e1436ec
Maël <nison.mael@gmail.com>
2016-09-18 18:50:18 +0200

    Install sequelize

--- sources/server.js
+++ sources/server.js
@@ -1,9 +1,16 @@
 import historyApiFallback                                                       from 'connect-history-api-fallback';
 import express                                                                  from 'express';
+import Sequelize                                                                from 'sequelize';
 import webpack                                                                  from 'webpack';
 import webpackDevMiddleware                                                     from 'webpack-dev-middleware';
 
+let database = new Sequelize({ dialect: `sqlite`, storage: `:memory:` });
+
 Promise.resolve().then(() => {
+
+    return database.sync();
+
+}).then(() => {
 
     return new Promise((resolve, reject) => {
 
