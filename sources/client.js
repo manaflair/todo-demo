1ae6d44fd16ece6ff09467fd61bdb9e46b5b6274
Maël <nison.mael@gmail.com>
2016-09-18 21:48:25 +0200

    Install redux & redux-saga

--- sources/client.js
+++ sources/client.js
@@ -1,10 +1,20 @@
 import ReactDOM                                                                 from 'react-dom';
+import { Provider as StoreProvider }                                            from 'react-redux';
+import createSagaMiddleware                                                     from 'redux-saga';
+import { applyMiddleware, combineReducers, createStore }                        from 'redux';
 
 import { Application }                                                          from 'components/Application';
+
+let sagaMiddleware = createSagaMiddleware();
+
+let reducer = combineReducers({});
+let enhancer = applyMiddleware(sagaMiddleware);
+
+let store = createStore(reducer, enhancer);
 
 let main = document.createElement('div');
 document.body.appendChild(main);
 
-ReactDOM.render(
+ReactDOM.render(<StoreProvider store={store}>
     <Application />
-, main);
+</StoreProvider>, main);
