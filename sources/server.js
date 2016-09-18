018ea218583a1f02303fdd1f0f84d37f224ccaba
Maël <nison.mael@gmail.com>
2016-09-18 18:53:25 +0200

    Define sequelize models

--- sources/server.js
+++ sources/server.js
@@ -1,10 +1,22 @@
 import historyApiFallback                                                       from 'connect-history-api-fallback';
 import express                                                                  from 'express';
+import { BOOLEAN, STRING, UUID, UUIDV4 }                                        from 'sequelize';
 import Sequelize                                                                from 'sequelize';
 import webpack                                                                  from 'webpack';
 import webpackDevMiddleware                                                     from 'webpack-dev-middleware';
 
 let database = new Sequelize({ dialect: `sqlite`, storage: `:memory:` });
+
+let Section = database.define(`Section`, {
+    id: { type: UUID, primaryKey: true, defaultValue: UUIDV4 },
+    title: { type: STRING, allowNull: false, defaultValue: `` }
+});
+
+let Note = database.define(`Note`, {
+    id: { type: UUID, primaryKey: true, defaultValue: UUIDV4 },
+    content: { type: STRING, allowNull: false, defaultValue: `` },
+    status: { type: BOOLEAN, allowNul: false, defaultValue: false }
+});
 
 Promise.resolve().then(() => {
 
